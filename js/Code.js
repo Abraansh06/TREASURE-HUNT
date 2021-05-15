d4b260733c8d7dbcefdf61d1901aa1bf76965679
Abraansh06 <75158624+Abraansh06@users.noreply.github.com>
2021-05-15 15:30:25 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+//Q2
+fill("white")
+    textSize(15)
+    text("CUTNIFON", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+    //Q3
+    fill("white")
+    textSize(15)
+    text("ATBDSAEA", 100,350)
+    fill("lightblue")
+    text("Hint: Stores Information !!", 100,370)
+
+
+    
+
+}
